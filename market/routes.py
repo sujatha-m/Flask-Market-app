667343ab9aa9c3e6a42d40050b4181168dc079ec
sujatha-m <jhanavi.bhushan@gmail.com>
2022-06-16 15:08:48 -0400

    Impletemted form page to register

--- market/routes.py
+++ market/routes.py
@@ -2,6 +2,7 @@
 from market import app
 from flask import render_template
 from market.models import Item
+from market.forms import RegisterForm
 @app.route("/")
 @app.route("/home")
 def home_page():
@@ -11,4 +12,9 @@ def home_page():
 def market_page():
     items = Item.query.all()
     return render_template("market.html", items=items)
+
+@app.route('/register')
+def register_page():
+    form = RegisterForm()
+    return render_template('register.html', form=form)
     

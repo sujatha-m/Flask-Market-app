37d99f8da7952ef52dd1e9e3b4c49a97ea7b6353
sujatha-m <jhanavi.bhushan@gmail.com>
2022-06-20 15:36:00 -0400

    added bcrypt to hash password

--- market/routes.py
+++ market/routes.py
@@ -1,5 +1,3 @@
-
-import re
 from market import app
 from flask import flash, render_template, redirect, url_for, flash, get_flashed_messages
 from market.models import Item, User
@@ -21,7 +19,7 @@ def register_page():
     if form.validate_on_submit():
         user_to_create = User(username=form.username.data,
                               email_address = form.email_address.data,
-                              password_hash= form.password1.data)
+                              password= form.password1.data)
         db.session.add(user_to_create)
         db.session.commit()
         return redirect(url_for('market_page'))
